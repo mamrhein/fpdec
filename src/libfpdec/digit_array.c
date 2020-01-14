3f18333ef77b62cec688726743a350858053dab5
Michael Amrhein <michael@adrhinum.de>
2020-01-14 17:58:24 +0100

    Fixed constant macros.

--- src/libfpdec/digit_array.c
+++ src/libfpdec/digit_array.c
@@ -14,15 +14,16 @@ $Revision$
 */
 
 
+
 #include "digit_array.h"
 
 /*****************************************************************************
 *  Macros
 *****************************************************************************/
 
-#define BITS_PER_DIGIT = 64
-#define BYTES_PER_DIGIT = 8                     // BITS_PER_DIGIT / 8
-#define DEC_DIGITS_PER_DIGIT = 19               // int(log10(2 ** 64))
-#define RADIX = 10000000000000000000UL          // 10 ** DEC_DIGITS_PER_DIGIT
-#define MAX_DIGIT = 9999999999999999999UL       // RADIX - 1
-#define MAX_DIGIT_T = 18446744073709551615UL    // 2 ** BITS_PER_DIGIT - 1
+#define BITS_PER_DIGIT 64
+#define BYTES_PER_DIGIT 8                       // BITS_PER_DIGIT / 8
+#define DEC_DIGITS_PER_DIGIT 19                 // int(log10(2 ** 64))
+#define RADIX 10000000000000000000UL            // 10 ** DEC_DIGITS_PER_DIGIT
+#define MAX_DIGIT 9999999999999999999UL         // RADIX - 1
+#define MAX_DIGIT_T 18446744073709551615UL      // 2 ** BITS_PER_DIGIT - 1
