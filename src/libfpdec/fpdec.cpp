c60a7942545c2b4017a39b739eda45e35969a153
Michael Amrhein <michael@adrhinum.de>
2020-06-14 22:31:36 +0200

    Prep for adding C++ API.

---
+++ src/libfpdec/fpdec.cpp
@@ -0,0 +1,15 @@
+/* ---------------------------------------------------------------------------
+Name:        fpdec.cpp
+
+Author:      Michael Amrhein (michael@adrhinum.de)
+
+Copyright:   (c) 2020 ff. Michael Amrhein
+License:     This program is part of a larger application. For license
+             details please read the file LICENSE.TXT provided together
+             with the application.
+------------------------------------------------------------------------------
+$Source:$
+$Revision:$
+*/
+
+#include "fpdec.hpp"
---
+++ src/libfpdec/fpdec.hpp
@@ -0,0 +1,18 @@
+/* ---------------------------------------------------------------------------
+Name:        fpdec.hpp
+
+Author:      Michael Amrhein (michael@adrhinum.de)
+
+Copyright:   (c) 2020 ff. Michael Amrhein
+License:     This program is part of a larger application. For license
+             details please read the file LICENSE.TXT provided together
+             with the application.
+------------------------------------------------------------------------------
+$Source:$
+$Revision:$
+*/
+
+#ifndef FPDEC_FPDEC_HPP
+#define FPDEC_FPDEC_HPP
+
+#endif //FPDEC_FPDEC_HPP
