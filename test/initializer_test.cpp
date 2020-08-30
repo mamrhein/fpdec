862fc935c1321bb2cdc35b813f49d6f912f9ccb4
Michael Amrhein <michael@adrhinum.de>
2020-08-30 16:54:26 +0200

    Added test cases.

--- test/initializer_test.cpp
+++ test/initializer_test.cpp
@@ -34,54 +34,54 @@ TEST_CASE("Initialize from ascii literal") {
     SECTION("Length of coeff <= MAX_N_DEC_DIGITS_IN_SHINT and "
             "(dec_prec <= MAX_DEC_PREC_FOR_SHINT or result == 0)") {
         struct test_data tests[] = {
-            {
-                .literal = "  000000000000001926.83 \n",
-                .sign = 1,
-                .dec_prec = 2,
-                .digits = {192683UL, 0UL}
-            },
-            {
-                .literal = "+5.387E+1",
-                .sign = 1,
-                .dec_prec = 2,
-                .digits = {5387UL, 0UL}
-            },
-            {
-                .literal = "-12345678901234567890e-7",
-                .sign = -1,
-                .dec_prec = 7,
-                .digits = {12345678901234567890UL, 0UL}
-            },
-            {
-                .literal = "82345678901234567890e-9",
-                .sign = 1,
-                .dec_prec = 9,
-                .digits = {8558702606396361426UL, 4UL}
-            },
-            {
-                .literal = "+007e28",
-                .sign = 1,
-                .dec_prec = 0,
-                .digits = {12899172069043863552UL, 3794707603UL}
-            },
-            {
-                .literal = "   +0.00e-0",
-                .sign = 0,
-                .dec_prec = 2,
-                .digits = {0UL, 0UL}
-            },
-            {
-                .literal = "-000000e5",
-                .sign = 0,
-                .dec_prec = 0,
-                .digits = {0UL, 0UL}
-            },
-            {
-                .literal = "-000000e-10",
-                .sign = 0,
-                .dec_prec = 10,
-                .digits = {0UL, 0UL}
-            },
+                {
+                        .literal = "  000000000000001926.83 \n",
+                        .sign = 1,
+                        .dec_prec = 2,
+                        .digits = {192683UL, 0UL}
+                },
+                {
+                        .literal = "+5.387E+1",
+                        .sign = 1,
+                        .dec_prec = 2,
+                        .digits = {5387UL, 0UL}
+                },
+                {
+                        .literal = "-12345678901234567890e-7",
+                        .sign = -1,
+                        .dec_prec = 7,
+                        .digits = {12345678901234567890UL, 0UL}
+                },
+                {
+                        .literal = "82345678901234567890e-9",
+                        .sign = 1,
+                        .dec_prec = 9,
+                        .digits = {8558702606396361426UL, 4UL}
+                },
+                {
+                        .literal = "+007e28",
+                        .sign = 1,
+                        .dec_prec = 0,
+                        .digits = {12899172069043863552UL, 3794707603UL}
+                },
+                {
+                        .literal = "   +0.00e-0",
+                        .sign = 0,
+                        .dec_prec = 2,
+                        .digits = {0UL, 0UL}
+                },
+                {
+                        .literal = "-000000e5",
+                        .sign = 0,
+                        .dec_prec = 0,
+                        .digits = {0UL, 0UL}
+                },
+                {
+                        .literal = "-000000e-10",
+                        .sign = 0,
+                        .dec_prec = 10,
+                        .digits = {0UL, 0UL}
+                },
         };
 
         for (const auto &test : tests) {
@@ -102,30 +102,38 @@ TEST_CASE("Initialize from ascii literal") {
 
     SECTION("Dec_prec > MAX_DEC_PREC_FOR_SHINT") {
         struct test_data tests[] = {
-            {
-                .literal = "+5.387E-17",
-                .sign = 1,
-                .dec_prec = 20,
-                .exp = -2,
-                .n_digits = 2,
-                .digits = {7000000000000000000UL, 538UL}
-            },
-            {
-                .literal = "82345678901234567890e-12",
-                .sign = 1,
-                .dec_prec = 12,
-                .exp = -1,
-                .n_digits = 2,
-                .digits = {9012345678900000000UL, 82345678UL}
-            },
-            {
-                .literal = "-3.0e-9",
-                .sign = -1,
-                .dec_prec = 10,
-                .exp = -1,
-                .n_digits = 1,
-                .digits = {30000000000UL}
-            }
+                {
+                        .literal = "+5.387E-17",
+                        .sign = 1,
+                        .dec_prec = 20,
+                        .exp = -2,
+                        .n_digits = 2,
+                        .digits = {7000000000000000000UL, 538UL}
+                },
+                {
+                        .literal = "82345678901234567890e-12",
+                        .sign = 1,
+                        .dec_prec = 12,
+                        .exp = -1,
+                        .n_digits = 2,
+                        .digits = {9012345678900000000UL, 82345678UL}
+                },
+                {
+                        .literal = "1e459",
+                        .sign = 1,
+                        .dec_prec = 0,
+                        .exp = 24,
+                        .n_digits = 1,
+                        .digits = {1000UL}
+                },
+                {
+                        .literal = "-3.0e-9",
+                        .sign = -1,
+                        .dec_prec = 10,
+                        .exp = -1,
+                        .n_digits = 1,
+                        .digits = {30000000000UL}
+                }
         };
 
         for (const auto &test : tests) {
@@ -151,73 +159,73 @@ TEST_CASE("Initialize from ascii literal") {
 
     SECTION("Length of coeff > MAX_N_DEC_DIGITS_IN_SHINT") {
         struct test_data tests[] = {
-            {
-                .literal = "  0001926.837209e26",
-                .sign = 1,
-                .dec_prec = 0,
-                .exp = 1,
-                .n_digits = 1,
-                .digits = {19268372090UL}
-            },
-            {
-                .literal = "-11702439610000.0000002938162540",
-                .sign = -1,
-                .dec_prec = 16,
-                .exp = -1,
-                .n_digits = 2,
-                .digits = {2938162540000UL, 11702439610000UL}
-            },
-            {
-                .literal = "0.0000000000000000000000000000000005",
-                .sign = 1,
-                .dec_prec = 34,
-                .exp = -2,
-                .n_digits = 1,
-                .digits = {50000UL}
-            },
-            {
-                .literal = "1111111111111111111"
-                           "2222222222222222222"
-                           "3333333333333333333"
-                           "4444444444444444444"
-                           "5555555555555555555"
-                           "."
-                           "6666666666666666666"
-                           "7777777777777777777"
-                           "888",
-                .sign = 1,
-                .dec_prec = 41,
-                .exp = -3,
-                .n_digits = 8,
-                .digits = {8880000000000000000UL,
-                           7777777777777777777UL,
-                           6666666666666666666UL,
-                           5555555555555555555UL,
-                           4444444444444444444UL,
-                           3333333333333333333UL,
-                           2222222222222222222UL,
-                           1111111111111111111UL}
-            },
-            {
-                .literal =
-                "-53095100000000000000000000000000000000000000"
-                ".00",
-                .sign = -1,
-                .dec_prec = 2,
-                .exp = 2,
-                .n_digits = 1,
-                .digits = {530951UL}
-            },
-            {
-                .literal = "792281625142643375935439504.11",
-                .sign = 1,
-                .dec_prec = 2,
-                .exp = -1,
-                .n_digits = 3,
-                .digits = {1100000000000000000UL,
-                           5142643375935439504UL,
-                           79228162UL}
-            },
+                {
+                        .literal = "  0001926.837209e26",
+                        .sign = 1,
+                        .dec_prec = 0,
+                        .exp = 1,
+                        .n_digits = 1,
+                        .digits = {19268372090UL}
+                },
+                {
+                        .literal = "-11702439610000.0000002938162540",
+                        .sign = -1,
+                        .dec_prec = 16,
+                        .exp = -1,
+                        .n_digits = 2,
+                        .digits = {2938162540000UL, 11702439610000UL}
+                },
+                {
+                        .literal = "0.0000000000000000000000000000000005",
+                        .sign = 1,
+                        .dec_prec = 34,
+                        .exp = -2,
+                        .n_digits = 1,
+                        .digits = {50000UL}
+                },
+                {
+                        .literal = "1111111111111111111"
+                                   "2222222222222222222"
+                                   "3333333333333333333"
+                                   "4444444444444444444"
+                                   "5555555555555555555"
+                                   "."
+                                   "6666666666666666666"
+                                   "7777777777777777777"
+                                   "888",
+                        .sign = 1,
+                        .dec_prec = 41,
+                        .exp = -3,
+                        .n_digits = 8,
+                        .digits = {8880000000000000000UL,
+                                   7777777777777777777UL,
+                                   6666666666666666666UL,
+                                   5555555555555555555UL,
+                                   4444444444444444444UL,
+                                   3333333333333333333UL,
+                                   2222222222222222222UL,
+                                   1111111111111111111UL}
+                },
+                {
+                        .literal =
+                        "-53095100000000000000000000000000000000000000"
+                        ".00",
+                        .sign = -1,
+                        .dec_prec = 2,
+                        .exp = 2,
+                        .n_digits = 1,
+                        .digits = {530951UL}
+                },
+                {
+                        .literal = "792281625142643375935439504.11",
+                        .sign = 1,
+                        .dec_prec = 2,
+                        .exp = -1,
+                        .n_digits = 3,
+                        .digits = {1100000000000000000UL,
+                                   5142643375935439504UL,
+                                   79228162UL}
+                },
         };
 
         for (const auto &test : tests) {
@@ -243,15 +251,15 @@ TEST_CASE("Initialize from ascii literal") {
 
     SECTION("Invalid ascii input") {
         std::string literals[] = {
-            " 1.23.5", "1.24e", "--4.92", "", "   ", "3,49E-3",
-            "\t+   \r\n"
+                " 1.23.5", "1.24e", "--4.92", "", "   ", "3,49E-3",
+                "\t+   \r\n"
         };
         for (const auto &literal : literals) {
             fpdec_t fpdec = FPDEC_ZERO;
 
             SECTION(literal) {
                 CHECK(fpdec_from_ascii_literal(&fpdec, literal.c_str()) ==
-                      FPDEC_INVALID_DECIMAL_LITERAL);
+                              FPDEC_INVALID_DECIMAL_LITERAL);
             }
         }
     }
@@ -269,10 +277,10 @@ TEST_CASE("Initialize from unicode literal") {
     error_t rc;
 
     for (const auto offset : offsets) {
-        snprintf((char *)(&buf), 30, "0x%08X", offset);
+        snprintf((char *) (&buf), 30, "0x%08X", offset);
         r = offset % 7;
         sign = r < 4 ? FPDEC_SIGN_NEG : FPDEC_SIGN_POS;
-        literal = (wchar_t *)fpdec_mem_alloc(r + 12, sizeof(wchar_t));
+        literal = (wchar_t *) fpdec_mem_alloc(r + 12, sizeof(wchar_t));
         for (int i = 0; i < r; ++i)
             literal[i] = ' ';
         literal[r] = sign < 0 ? '-' : '+';
@@ -300,7 +308,7 @@ TEST_CASE("Initialize from long long.") {
     char buf[30];
 
     for (const auto test_val : test_vals) {
-        snprintf((char *)(&buf), 30, "%lld", test_val);
+        snprintf((char *) (&buf), 30, "%lld", test_val);
         fpdec_t *fpdec = fpdec_new();
         assert(fpdec != NULL);
         error_t rc = fpdec_from_long_long(fpdec, test_val);
@@ -340,18 +348,18 @@ TEST_CASE("Initialize from sign, digits, exp.") {
     };
 
     struct test_data test_vals[] = {
-        {FPDEC_SIGN_POS, 1, {823,}, 0, false, 0, {823,}, 0},
-        {FPDEC_SIGN_POS, 5, {27, 5, 0, 0, 0,}, 0, true, 2, {27, 5,}, 0},
-        {FPDEC_SIGN_NEG, 5, {0, 76629, 0, 0, 0,}, -1, false, 0, {76629,}, 0},
-        {FPDEC_SIGN_NEG, 5, {0, 76629, 333, 0, 0,}, -4,
-         true, 2, {76629, 333,}, -3},
-        {FPDEC_SIGN_NEG, 7, {1, 2, 3, 4, 5, 6, 7}, -3,
-         true, 7, {1, 2, 3, 4, 5, 6, 7}, -3},
+            {FPDEC_SIGN_POS, 1, {823,},                       0,  false, 0, {823,},                    0},
+            {FPDEC_SIGN_POS, 5, {27, 5,     0,   0, 0,},      0,  true,  2, {27,    5,},               0},
+            {FPDEC_SIGN_NEG, 5, {0,  76629, 0,   0, 0,},      -1, false, 0, {76629,},                  0},
+            {FPDEC_SIGN_NEG, 5, {0,  76629, 333, 0, 0,},      -4,
+                                                                  true,  2, {76629, 333,},             -3},
+            {FPDEC_SIGN_NEG, 7, {1,  2,     3,   4, 5, 6, 7}, -3,
+                                                                  true,  7, {1,     2, 3, 4, 5, 6, 7}, -3},
     };
     char buf[30];
 
     for (struct test_data test_val : test_vals) {
-        snprintf((char *)(&buf), 30, "%c %zu: %lu ...",
+        snprintf((char *) (&buf), 30, "%c %zu: %lu ...",
                  test_val.sign < 0 ? '-' : '+',
                  test_val.res_n_digits, test_val.res_digits[0]);
         fpdec_t *fpdec = fpdec_new();
@@ -371,11 +379,11 @@ TEST_CASE("Initialize from sign, digits, exp.") {
                     CHECK(FPDEC_DEC_PREC(fpdec) == 0);
                 else
                     CHECK(FPDEC_DEC_PREC(fpdec) ==
-                          -test_val.res_exp * DEC_DIGITS_PER_DIGIT);
+                                  -test_val.res_exp * DEC_DIGITS_PER_DIGIT);
                 CHECK(FPDEC_DYN_N_DIGITS(fpdec) == test_val.res_n_digits);
                 for (int idx = 0; idx > test_val.res_n_digits; ++idx)
                     CHECK(FPDEC_DYN_DIGITS(fpdec)[idx] ==
-                          test_val.res_digits[idx]);
+                                  test_val.res_digits[idx]);
             }
             else {
                 CHECK(is_shint(fpdec));
